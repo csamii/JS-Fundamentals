38d47d71d860c03682b23ba8fc60a8ff91e755e9
csamii <csamii27@gmail.com>
2025-05-20 07:47:47 -0700

    Using argv in node

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-const arg = process.argv[2]
-if (arg) {
+const arg = process.argv.length;
+if (arg > 2) {
     console.log("Argument found");
 } else {
     console.log("No argument");
